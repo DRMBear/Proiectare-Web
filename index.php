ce5e62403dcbcc42a4613718c9fd4293efd2e473
DRMBear <57721236+DRMBear@users.noreply.github.com>
2020-10-15 21:47:20 +0300

    index3

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<?php  
-$num = 0;  
-$n1 = 0;  
-$n2 = 1;  
-echo "<h3>Fibonacci series for first 12 numbers: </h3>";  
-echo "\n";  
-echo $n1.' '.$n2.' ';  
-while ($num < 10 )  
-{  
-    $n3 = $n2 + $n1;  
-    echo $n3.' ';  
-    $n1 = $n2;  
-    $n2 = $n3;  
-    $num = $num + 1;  
-?>  
+<?php   
+// PHP code to get the Fibonacci series 
+// Recursive function for fibonacci series. 
+function Fibonacci($number){ 
+      
+    // if and else if to generate first two numbers 
+    if ($number == 0) 
+        return 0;     
+    else if ($number == 1) 
+        return 1;     
+      
+    // Recursive Call to get the upcoming numbers 
+    else
+        return (Fibonacci($number-1) +  
+                Fibonacci($number-2)); 
+} 
+  
+// Driver Code 
+$number = 10; 
+for ($counter = 0; $counter < $number; $counter++){   
+    echo Fibonacci($counter),' '; 
+} 
+?> 
